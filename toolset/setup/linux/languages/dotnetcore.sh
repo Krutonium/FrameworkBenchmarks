e557bbcdcf08c0a553efffa0dfc0b5dfcae87fa5
Doug Bunting <dougbu@microsoft.com>
2017-05-31 16:18:58 -0700

    Go back to .NET Core SDK 1.1.3 and the ASP.NET Core 1.1.3 patch train - revert parts of "Use .NET Core 2.0.0-preview1" and "Address PR and offline comments"   - commits b9a4fc522d5836fb6e90e4a10159110060ae4c5d and 81e6764a98e4105f8d6b5228b5fc46cf6cf90d82   - still need `NoTransactionSqlServerConnection`   - no compiled queries yet   - no `WebHostBuilder.UseDefaultServiceProvider()` yet   - still use `PlatformAbstractions`   - maintain `Content-Length` hack   - back to WebListener from HttpSys

--- toolset/setup/linux/languages/dotnetcore.sh
+++ toolset/setup/linux/languages/dotnetcore.sh
@@ -7,7 +7,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk-2.0.0
+sudo apt-get install -y dotnet-dev-1.1.3
 
 echo "PATH=$HOME/.dotnet:$PATH" > $IROOT/dotnetcore.installed
 
